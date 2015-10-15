6b4c8b366907f8405d37df80a1e28bbc02d42b30
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2015-10-15 13:03:16 +0200

    Bluetooth communication

--- app/src/main/java/info/korzeniowski/rcontroller/MainActivity.java
+++ app/src/main/java/info/korzeniowski/rcontroller/MainActivity.java
@@ -1,11 +1,16 @@
 package info.korzeniowski.rcontroller;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,10 +18,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -27,18 +38,21 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     Toolbar toolbar;
     @Bind(R.id.fab)
     FloatingActionButton fab;
+    @Bind(R.id.fab1)
+    FloatingActionButton fab1;
     @Bind(R.id.text)
     TextView text;
+    @Bind(R.id.slider)
+    SeekBar slider;
 
     private SensorManager mSensorManager;
 
-    private static final float NS2S = 1.0f / 1000000000.0f;
-    protected float[] vGyroscope = new float[3];
-    private double timestamp;
-    private long timeStampGyroscope = 0;
-    private float[] deltaRotationVector = new float[4];
-    private float[] sum = new float[3];
     private GyroscopeValues initialRotationVectors;
+    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothDevice mmDevice;
+    private BluetoothSocket mmSocket;
+    private OutputStream mmOutputStream;
+    private InputStream mmInputStream;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,8 +77,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
                     }
                 };
-
-                mSensorManager.registerListener(initRotationVectors, mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_FASTEST);
+                mSensorManager.registerListener(initRotationVectors, mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_GAME);
             }
         });
 
@@ -74,7 +87,70 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR), SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR), SensorManager.SENSOR_DELAY_GAME);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth, 0);
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                if (device.getName().equals("HC-05")) {
+                    mmDevice = device;
+                    break;
+                }
+            }
+        }
+
+        try {
+            UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); //Standard SerialPortService ID
+            mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+            mmSocket.connect();
+            mmOutputStream = mmSocket.getOutputStream();
+            mmInputStream = mmSocket.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        slider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                try {
+                    String msg = getBluetoothMsg(progress);
+                    text.setText(msg);
+                    mmOutputStream.write(msg.getBytes());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    @NonNull
+    private String getBluetoothMsg(int progress) {
+        StringBuilder sb = new StringBuilder("^");
+        if (progress < 50) {
+            sb.append(2).append(" ").append((50 - progress) * 2);
+        } else if (progress <= 100) {
+            sb.append(1).append(" ").append((progress - 50) * 2);
+        }
+
+        sb.append("|").append(50).append("$");
+        return sb.toString();
     }
 
     @Override
@@ -85,16 +161,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -108,14 +180,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent event) {
         GyroscopeValues values = new GyroscopeValues(event.values);
+
+
         if (initialRotationVectors == null) {
             initialRotationVectors = values;
         }
 
         float deltaAxisZ = values.axisZ - initialRotationVectors.axisZ;
         int pwmFillness = convertToPwm(deltaAxisZ);
-
-        text.setText("" + deltaAxisZ + " ; " + pwmFillness);
     }
 
     private int convertToPwm(float delta) {
