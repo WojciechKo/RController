93def87668548f568ffea5360528f31d8946387e
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2015-11-02 02:24:15 +0100

    Arduino update

--- arduino/rcController/rcController.ino
+++ arduino/rcController/rcController.ino
@@ -1,32 +1,37 @@
 #include <SoftwareSerial.h>
 #include <Servo.h>
 
-#define bluetoothTx 3
-#define bluetoothRx 2
-#define enginePin 5
-#define servoPin 6
+#define bluetoothTx 2
+#define bluetoothRx 3
+#define engineForwardPin 4
+#define engineBackwardPin 5
+#define enginePowerPin 6
+#define servoPin 9
 
 Servo servo;
 SoftwareSerial bluetooth(bluetoothRx, bluetoothTx);
 
-int side = 1;
 const int SIDE_RIGHT = 1;
 const int SIDE_LEFT = 2;
 
+int side = SIDE_RIGHT;
 int angle = 0;
 
-int direction = 1;
+int servoFix = 50;
+
 const int DIRECTION_FORWARD = 1;
 const int DIRECTION_BACKWARD = 2;
 
+int direction = DIRECTION_FORWARD;
 int speed = 0;
 
 int enginePwm = 0;
 int servoPwm = 90;
+const int MIN_ENGINE_PWM = 0;
 
 // cmd:
-// ^ [1|2] [0-100] | [1|2] [0-100] $
-//    side  angle  direction  speed
+// ^ [1|2] [0-100] | [1|2] [0-100] [0-100]$
+// ^direction speed | side   angle  servo-fix$
 char START_CMD_CHAR = '^';
 char END_CMD_CHAR = '$';
 char SEP_CMD_CHAR = '|';
@@ -35,6 +40,11 @@ void setup() {
   Serial.begin(9600);
   bluetooth.begin(9600);
   servo.attach(servoPin);
+  
+  digitalWrite(engineForwardPin, HIGH);
+  digitalWrite(engineBackwardPin, LOW);
+  analogWrite(servoPin, getServoPwm(side, angle, servoFix));
+  analogWrite(enginePowerPin, getEnginePwm(direction, speed));
 }
 
 void loop() {
@@ -47,12 +57,11 @@ void loop() {
 
   int newDirection = bluetooth.parseInt();
   int newSpeed = bluetooth.parseInt();
-
   if (bluetooth.read() != SEP_CMD_CHAR) return;
 
   int newSide = bluetooth.parseInt();
   int newAngle = bluetooth.parseInt();
-
+  int servoFix = bluetooth.parseInt();
   if (bluetooth.read() != END_CMD_CHAR) return;
 
   // Validation
@@ -70,10 +79,12 @@ void loop() {
   Serial.print(newSide);
   Serial.print(" ");
   Serial.print(newAngle);
+  Serial.print(" ");
+  Serial.print(servoFix);
   Serial.println(END_CMD_CHAR);
 
   // Handle servo data
-  int newServoPwm = getServoPwm(newSide, newAngle);
+  int newServoPwm = getServoPwm(newSide, newAngle, servoFix);
   if (servoPwm != newServoPwm) {
     servoPwm = newServoPwm;
     servo.write(servoPwm);
@@ -82,41 +93,56 @@ void loop() {
   
   // Handle engine data
   if (direction != newDirection) {
-    for (int i = speed ; i >= 0 ; i--) {
+    // slow down
+    for (int i = speed ; i >= MIN_ENGINE_PWM ; i--) {
       delay(10);
       enginePwm = getEnginePwm(direction, i);
-      analogWrite(enginePin, enginePwm);
+      analogWrite(enginePowerPin, enginePwm);
     }
 
-    for (int i = 0 ; i <= newSpeed ; i++) {
+    // set direction
+    digitalWrite(engineForwardPin, newDirection == DIRECTION_FORWARD ? HIGH : LOW);
+    digitalWrite(engineBackwardPin, newDirection == DIRECTION_BACKWARD ? HIGH : LOW);
+    direction = newDirection;
+    
+    // speed up
+    for (int i = MIN_ENGINE_PWM ; i <= newSpeed ; i++) {
       delay(10);
       enginePwm = getEnginePwm(newDirection, i);
-      analogWrite(enginePin, enginePwm);
+      analogWrite(enginePowerPin, enginePwm);
     }
-
-    direction = newDirection;
     speed = newSpeed;
+    
     Serial.println("Direction changed");
   }
 
-  // Changed only speed
+  // Changed speed only
   if (speed != newSpeed) {
     speed = newSpeed;
     enginePwm = getEnginePwm(direction, speed);
-    analogWrite(enginePin, enginePwm);
-    Serial.println("Speed changed");
+    analogWrite(enginePowerPin, enginePwm);
+    Serial.print("Speed changed: ");
+    Serial.println(enginePwm);
   }
 }
 
-int getServoPwm(int side, int angle) {
+// Returns [0 - 180]
+int getServoPwm(int side, int angle, int servoFix) {
   angle *= 0.9;
-  if (side == SIDE_RIGHT) {
-    return 90 - angle;
-  } else { // SIDE_LEFT
-    return 90 + angle;
+  int result = 90;
+  switch (side) {
+    case SIDE_RIGHT:
+      result -= angle;
+      break;
+    case SIDE_LEFT:
+      result += angle;
+      break;
   }
+  result += servoFix;
+  return result;
 }
 
+// Returns [0 - 255]
 int getEnginePwm(int direction, int speed) {
   return speed * 2.55;
 }
